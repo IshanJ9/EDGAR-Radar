c9636dbaab5fa68b457a21d9c7295e979a1e18b8
IshanJ <ishanjadhav04@gmail.com>
2026-09-09 18:45:32 +0530

    set up node-pg-migrate, add companies table migration

---
+++ migrations/1788959633418_companies-table.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('companies', {
+    cik: { type: 'char(10)', primaryKey: true },
+    entity_name: { type: 'text', notNull: true },
+    created_at: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
+    updated_at: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('companies');
+};
